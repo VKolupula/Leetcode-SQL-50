91c721692af3eda0e94fdb16d08c1236dfc7d30d
Vikram <120835150+VKolupula@users.noreply.github.com>
2024-02-12 18:14:14 +0000

    Update 1934. Confirmation Rate.sql

--- 1934. Confirmation Rate.sql
+++ 1934. Confirmation Rate.sql
@@ -1,3 +1,4 @@
-select s.user_id, round(count(case when cc.action = 'confirmed' then 1 end)*1.00 / (case when count(cc.user_id) = 0 then 1 else count(cc.user_id) end),2) as confirmation_rate
+select s.user_id, round(count(case when cc.action = 'confirmed' then 1 end)*1.00 / 
+(case when count(cc.user_id) = 0 then 1 else count(cc.user_id) end),2) as confirmation_rate
 from Confirmations cc right join Signups s on s.user_id = cc.user_id
-group by s.user_id;
+group by s.user_id
